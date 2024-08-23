12a658d20dc4c8ef2d674ceee6a6d0227a0c118f
Rishav9803 <rishavjhaa20003@gmail.com>
2024-08-23 14:55:04 +0530

    Lecture-9 Added

---
+++ JAVA-SCRIPT/Lecture-9/1.IntroToJs/Variables.js
@@ -0,0 +1,12 @@
+var a = 10;
+console.log(a);
+
+let x = "Hello World!"
+console.log(x)
+
+y = "Learning JS"
+console.log(y);
+
+const pi = 3.14;
+//pi value cannot be changed because we have written it with const (Exm. pi = 3.)
+console.log(pi);
--- JAVA-SCRIPT/Lecture-9/1.IntroToJs/intro.js
+++
@@ -1,2 +0,0 @@
-let a = 444333;
-console.log(a)
