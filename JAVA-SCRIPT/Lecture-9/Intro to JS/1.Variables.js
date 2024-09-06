810fd7e686cc2491db0af9b3e875e18515514e9a
Rishav9803 <rishavjhaa20003@gmail.com>
2024-09-06 14:52:51 +0530

    DONE

---
+++ JAVA-SCRIPT/Lecture-9/Intro to JS/1.Variables.js
@@ -0,0 +1,19 @@
+var a = 10;
+console.log(a);
+
+let x = "Hello World"
+console.log(x);
+
+y="Learning JS"
+console.log(y);
+
+console.log(typeof a);
+console.log(typeof x);
+console.log(typeof y);
+
+// a = 5.566551;
+// b = true;
+// c = "Heloo this is a string"; //last one will print like from console.log
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
---
+++ JAVA-SCRIPT/Lecture-9/Intro to JS/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
